447b794d9d7cfa7c726d4d2771cf0392de117b8a
Shakhzod221 <130886474+Shakhzod221@users.noreply.github.com>
2023-08-20 11:45:02 +0500

    My Task

---
+++ main.js
@@ -0,0 +1,146 @@
+let football = [
+    {
+        name: 'Shaxzod',
+        age: 24,
+        study: true,
+        address: "Xorazm",
+    },
+
+    {
+        name: 'Messi',
+        the_number: 30,
+        study: true,
+        address: 'Saudiya Arab'
+    },
+
+    {
+        name: 'Ronoldo',
+        the_number: 7,
+        study: true,
+        adres: 'Mayami'
+    },
+
+    {
+        name: 'Neymar',
+        the_number: 11,
+        study: true,
+        adres: 'PSJ'
+    },
+
+    {
+        name: 'De Bryune',
+        the_number: 17,
+        study: true,
+        adres: 'Manchestr sity'
+    },
+
+    {
+        name: 'Mubappe',
+        the_number: 10,
+        study: true,
+        adres: 'PSJ'
+    },
+
+    {
+        name: 'Holland',
+        the_number: 9,
+        study: true,
+        adres: 'Manchest Sity'
+    },
+
+    {
+        name: 'Sterling',
+        the_number: 7,
+        study: true,
+        adres: 'Manchestr Sity'
+    },
+
+    {
+        name: 'Farrux',
+        the_number: 7,
+        study: false,
+        adres: 'Uzbekistan'
+    },
+
+    {
+        name: 'Xursand',
+        the_number: 10,
+        study: false,
+        adres: 'Uzbekistan'
+    },
+]
+
+
+
+
+            // Hosting
+
+console.log(num);
+
+var num = 20;
+
+
+            // Function
+
+    
+    // add(3, 4);
+
+
+    // function add(x, y){
+    //     return console.log(x + y);
+    // }
+
+
+
+    // add(10, 5);
+
+
+    // function add(x,y){
+    //     return console.log(x-y);
+    // }
+
+
+
+    // add(4,5);
+
+
+    // function add(x, y){
+    //     return console.log(x * y);
+    // }
+
+
+
+    // add(4, 2);
+
+
+    // function add(x, y){
+    //     return console.log(x / y);
+    // }
+
+
+
+
+                // if else
+
+
+    // let num = 20;
+
+    // if(num > 10){
+    //     console.log(true);
+    // }else{
+    //     console.log(false);
+    // }
+
+
+
+    // let age = 10;
+
+    // if(age > 20){
+    //     console.log('katta');
+    // }else{
+    //     console.log('kichkina');
+    // }
+
+
+
+    
